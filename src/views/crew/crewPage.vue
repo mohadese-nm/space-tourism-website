e43dfe662f068572dbeeffc7f781c51c5ed1b65a
mohadese-nm <mohadese.namavar@gmail.com>
2023-01-21 22:29:18 +0330

    fixed crew info text styles

--- src/views/crew/crewPage.vue
+++ src/views/crew/crewPage.vue
@@ -3,12 +3,10 @@
     <navigation-bar></navigation-bar>
     <div class="container">
       <div class="row">
-
         <div class=" col d-block crew-header">
           <span>02</span>
           <p class="crew-text">meet your crew</p>
         </div>
-
         <div class="col crew-info">
           <router-view></router-view>
         </div>
@@ -39,55 +37,57 @@ export default {
   background-image: url(@/assets/crew/background-crew-desktop.jpg);
   background-size: cover;
 
-  .crew-header {
-    position: absolute;
-    width: 382px;
-    height: 34px;
-    left: 166.5px;
-    top: 212px;
-
-    span {
-      width: 26px;
+  .container{
+    .crew-header {
       position: absolute;
+      width: 382px;
       height: 34px;
-      left: 0%;
-      right: 93.19%;
-      top: calc(50% - 34px/2);
-      font-family: $barlowCond;
-      font-style: normal;
-      font-weight: 700;
-      font-size: 28px;
-      line-height: 34px;
-      letter-spacing: 4.725px;
-      color: white;
-      mix-blend-mode: normal;
-      opacity: 0.25;
+      left: 15vw;
+      top: 212px;
+      
+      span {
+        width: 26px;
+        position: absolute;
+        height: 34px;
+        left: 0%;
+        right: 93.19%;
+        top: calc(50% - 34px/2);
+        font-family: $barlowCond;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 28px;
+        line-height: 34px;
+        letter-spacing: 4.725px;
+        color: white;
+        mix-blend-mode: normal;
+        opacity: 0.25;
+      }
+      
+      .crew-text {
+        position: absolute;
+        height: 34px;
+        width: 327px;
+        left: 14.14%;
+        right: 1.57%;
+        top: calc(50% - 34px/2);
+        font-family: $barlowCond;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 28px;
+        line-height: 34px;
+        letter-spacing: 4.725px;
+        text-transform: uppercase;
+        color: white;
+      }
     }
-
-    .crew-text {
+      
+    .crew-info {
       position: absolute;
-      height: 34px;
-      width: 327px;
-      left: 14.14%;
-      right: 1.57%;
-      top: calc(50% - 34px/2);
-      font-family: $barlowCond;
-      font-style: normal;
-      font-weight: 400;
-      font-size: 28px;
-      line-height: 34px;
-      letter-spacing: 4.725px;
-      text-transform: uppercase;
-      color: white;
+      left: 8vw;
+      top: 48vh;
+      width: 100%;
+      height: 47vh;
     }
-  }
-
-  .crew-info{
-    position: absolute;
-    left: 11vw;
-    top: 44vh;
-    width: 100%;
-    height: 47vh;
   }
 }
 </style>
--- src/views/crew/douglasPage.vue
+++ src/views/crew/douglasPage.vue
@@ -3,20 +3,20 @@
     <div class="row">
         <div class="col crew-info">
             <div class="row">
-                <div class="col d-block crew-role">
+                <div class="col-12 crew-role">
                     <p>commander</p>
                 </div>
-                <div class="col d-block crew-name">
+                <div class="col-12 crew-name">
                     <p>Douglas Hurley</p>
                 </div>
-                <div class="col d-block crew-about">
+                <div class="col-12 crew-about mt-2">
                     <p>
                         Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former 
                         NASA astronaut. He launched into space for the third time as commander of Crew 
                         Dragon Demo-2.
                     </p>
                 </div>
-                <div class="col d-block crew-nav">
+                <div class="col-12 crew-nav">
 
                 </div>
             </div>
@@ -36,5 +36,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use '@/assets/styles/-variables.scss' as *;
+.container{
+    .crew-info{
+        .crew-role{
+            font-family: $belle;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 32px;
+            line-height: 37px;
+            text-transform: uppercase;
+            color: #FFFFFF;
+            mix-blend-mode: normal;
+            opacity: 0.5;
+        }
 
+        .crew-name{
+            font-family: $belle;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 56px;
+            line-height: 64px;
+            text-transform: uppercase;
+            color: #FFFFFF;
+        }
+
+        .crew-about{
+            p{
+                width: 29.4vw;
+                font-family: 'Barlow';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 18px;
+                line-height: 32px;
+                /* or 178% */
+                color: #D0D6F9;
+            }
+        }
+    }
+}
 </style>
