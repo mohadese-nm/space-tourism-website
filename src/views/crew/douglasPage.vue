4856fbdec001bcb6848083de50ecc2acdf628617
mohadese-nm <mohadese.namavar@gmail.com>
2023-01-21 21:58:08 +0330

    add douglasPage.vue text content

--- src/views/crew/douglasPage.vue
+++ src/views/crew/douglasPage.vue
@@ -4,13 +4,17 @@
         <div class="col crew-info">
             <div class="row">
                 <div class="col d-block crew-role">
-
+                    <p>commander</p>
                 </div>
                 <div class="col d-block crew-name">
-
+                    <p>Douglas Hurley</p>
                 </div>
                 <div class="col d-block crew-about">
-
+                    <p>
+                        Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former 
+                        NASA astronaut. He launched into space for the third time as commander of Crew 
+                        Dragon Demo-2.
+                    </p>
                 </div>
                 <div class="col d-block crew-nav">
 
