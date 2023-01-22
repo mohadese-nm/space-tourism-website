4929dca5aaca10be29c6e35c83e4afb01acccb79
mohadese-nm <mohadese.namavar@gmail.com>
2023-01-22 23:39:56 +0330

    built crew navigation base and grid and color style

---
+++ src/views/crew/crewNav.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+        <div class="row align-items-center justify-content-center nav-links">
+            <div class="col link-column me-3">
+                <router-link :to="{ name: 'douglas' }">
+                    <div class="circle"></div>
+                </router-link>
+
+            </div>
+
+            <div class="col link-column me-3">
+                <router-link :to="{ name: 'mark' }">
+                    <div class="circle"></div>
+                </router-link>
+
+            </div>
+
+            <div class="col link-column me-3">
+                <router-link :to="{ name: 'victor' }">
+                    <div class="circle"></div>
+                </router-link>
+
+            </div>
+
+            <div class="col link-column">
+                <router-link :to="{ name: 'anousheh' }">
+                    <div class="circle"></div>
+                </router-link>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'crewNav'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/-variables.scss' as *;
+.nav-links{
+    width: 9vw;
+    height: 1vh;
+    .link-column{
+        text-align: center;
+        padding: 0;
+
+        .circle{
+            border-radius: 50%;
+            width: 15px;
+            height: 15px;
+            background-color: white;
+            opacity: 0.17;
+
+            &:hover {
+                opacity: 0.5;
+            }
+            
+            &.router-link-active {
+                opacity: 1;
+            }
+        }
+    }
+}
+</style>
--- src/views/crew/douglasPage.vue
+++ src/views/crew/douglasPage.vue
@@ -29,7 +29,7 @@
             </div>
             <div class="row">
                 <div class="col crew-nav">
-
+                    <crew-nav></crew-nav>
                 </div>
             </div>
         </div>
