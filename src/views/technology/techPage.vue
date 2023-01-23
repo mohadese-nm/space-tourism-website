8a624fac20d9de65ed78cba847af21e4328ce9ef
mohadese-nm <mohadese.namavar@gmail.com>
2023-01-23 23:15:23 +0330

    style techpage component

---
+++ src/views/technology/techPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content">
+    <navigation-bar></navigation-bar>
+    <div class="container">
+        <div class="tech-info">
+          <router-view></router-view>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import navigationBar from '../navigationBar.vue';
+export default {
+  name: 'techPage',
+  components: {
+    navigationBar,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/-variables.scss' as *;
+
+.content {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 900px;
+  background-image: url(@/assets/technology/background-technology-desktop.jpg);
+  background-size: cover;
+}
+</style>
