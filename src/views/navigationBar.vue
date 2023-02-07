e7d2373522f09d9c5b63cd99b557d2e6759ca818
mohadese-nm <mohadese.namavar@gmail.com>
2023-02-07 22:25:15 +0330

    change burger and close div to button

--- src/views/navigationBar.vue
+++ src/views/navigationBar.vue
@@ -10,18 +10,19 @@
             <div class="col nav-line">
                 <div class="line"></div>
             </div>
-            <div class="burger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
+            <button class="burger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21" aria-label="menu">
                     <g fill="#D0D6F9" fill-rule="evenodd">
                     <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z"/></g>
                 </svg>
-            </div>
-            <div class="close">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21"><g fill="#D0D6F9" fill-rule="evenodd">
+            </button>
+            <button class="close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21"><g fill="#D0D6F9" 
+                fill-rule="evenodd" aria-label="close menu">
                     <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/>
                     <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g>
                 </svg>
-            </div>
+            </button>
             <div class="col nav-blur">
                 <div class="nav-links">
                     
@@ -108,6 +109,7 @@
         width: 25px;
         height: 22px;
         padding: 0;
+        background: none;
     }
 
     .close{
