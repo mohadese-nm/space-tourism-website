9777f7017536303bdb476c01e66759583205874c
mohadese-nm <mohadese.namavar@gmail.com>
2023-02-16 03:44:14 +0330

    finnished moon tablet size

--- src/views/destination/moonPage.vue
+++ src/views/destination/moonPage.vue
@@ -1,38 +1,37 @@
 <template> 
   <div class="container">
-    <div class="row justify-content-between dest-content">
-      <div class="col-7 pt-5 dest-img">
+    <div class="row dest-content">
+      <div class="col-12 col-xl-7 dest-img">
         <img src="../../assets/destination/image-moon.webp" >
       </div>
 
-      <div class="col-5 dest-details">
+      <div class="col-12 col-xl-5 dest-details">
         <div class="row">
 
           <div class="col-12 dest-nav">
             <destination-nav></destination-nav>
           </div>
           
-          <div class="col-12 mt-5 dest-name">
+          <div class="col-12  dest-name">
             <p>MOON</p>
           </div>
 
-          <div class="col-12 mt-5 dest-description">
+          <div class="col-12  dest-description">
             <p>See our planet as you’ve never seen it before. A perfect relaxing 
               trip away to help regain perspective and come back
               refreshed. While you’re there, take in some history by visiting the 
               Luna 2 and Apollo 11 landing sites.</p>
           </div>
-          <div class="col-12 mt-5 line"></div>
+          <div class="col-12  line"></div>
 
           <div class="col-12 mt-3 dest-info">
-
-            <div class="row">
+            <div class="row distance">
               <div class="col-sm dest-distance">
                 <p class="info-title">AVG. DISTANCE</p>
                 <p class="info-number">384,400 km</p>
               </div>
 
-              <div class="col-sm dest-time ps-5">
+              <div class="col-sm dest-time">
                 <p class="info-title">Est. travel time</p>
                 <p class="info-number">3 days</p>
               </div>
@@ -60,10 +59,13 @@ export default {
 @use '@/assets/styles/-variables.scss' as *;
 .container{
   .dest-content{
+    display: flex;
+    justify-content: space-between;
     margin: 30vh auto 0 6vw;
     .dest-img{
       width: 445px;
       height: 445px;
+      padding-top: 7vh;
     }
 
   .dest-details{
@@ -71,6 +73,7 @@ export default {
     height: 472px;
     .dest-name {
         height: 115px;
+        margin-top: 7vh;
         p{
           font-family: $belle;
           font-style: normal;
@@ -82,6 +85,7 @@ export default {
       }
       .dest-description{
         height: 128px;
+        margin-top: 7vh;
         p{
           font-family: $barlow;
           font-style: normal;
@@ -96,6 +100,7 @@ export default {
         width: 436px;
         background-color: #383B4B;
         mix-blend-mode: normal;
+        margin-top: 7vh;
       }
       .dest-info{
         width: 370px;
@@ -126,6 +131,7 @@ export default {
         }
         .dest-time{
           height: 62px;
+          padding-left: 3vw;
           .info-title {
               height: 17px;
               font-family: $barlowCond;
@@ -150,9 +156,68 @@ export default {
             }
         }
       }
-  }
+    }
   }
 }
 
-
+@media screen and (max-width:1200px){
+  .container{
+    .dest-content{
+      margin: 16vh auto 0 auto;
+      max-width: 574px;
+      justify-content: center;
+      .dest-img{
+        max-width: 350px;
+        max-height: 350px;
+        padding-top: 0;
+        img{
+          width: 350px;
+          height: 350px;
+        }
+      }
+      .dest-details{
+        width: 573px;
+        height: fit-content;
+        margin-top: 5vh;
+        .dest-nav{
+          display: flex;
+          justify-content: center;
+        }
+        .dest-name{
+          margin-top: 4vh;
+          p{
+            font-size: 80px;
+            line-height: 92px;
+            text-align: center;
+          }
+        }
+        .dest-description{
+          margin-top: 3vh;
+          p{
+            height: 84px;
+            font-size: 16px;
+            line-height: 28px;
+            text-align: center;
+          }
+        }
+        .line{
+          margin: 2vh auto 0 auto;
+          width: 572px;
+        }
+        .dest-info{
+          width: 572px;
+          .distance{
+            justify-content: center;
+            .dest-distance{
+              text-align: center;
+            }
+            .dest-time{
+              text-align: center;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
